ff61a1b6f7034772ac7bc23449a0fe4ff75a1f02
Muhammad Ra'if Alkautsar <mraifalkautsar@gmail.com>
2026-02-02 00:19:48 +0700

    fix: env

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ const { Pool } = require("pg");
 
 // Database connection pool
 const pool = new Pool({
-  host: process.env.DB_HOST || 'db',
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  database: process.env.DB_NAME || 'nimonspedia_db',
-  user: process.env.DB_USER || 'nimons_user',
-  password: process.env.DB_PASSWORD || 'pass',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
